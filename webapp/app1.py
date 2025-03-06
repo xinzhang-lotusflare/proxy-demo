379caa90fafe123cae70a0d87cb0a2db6cfbd95b
Xin Zhang <xin.zhang@lotusflare.com>
2025-03-06 15:42:29 +0800

    add logger

--- webapp/app1.py
+++ webapp/app1.py
@@ -1,7 +1,31 @@
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, Request
 
-print("webapp-1 started")
 app = FastAPI()
+
+###
+# Event Logger
+###
+def create_logger(file=None):
+    json_fmt = '{"timestamp":"%(asctime)s","log_level":"%(levelname)s","module":"%(module)s","event":%(message)s}'
+    if file:
+        log_handler = logging.FileHandler(file)
+    else:
+        log_handler = logging.StreamHandler()
+    basic_json_fmt = logging.Formatter(
+        fmt=json_fmt,
+        datefmt="%Y-%m-%dT%H:%M:%S"
+    )
+    log_handler.setFormatter(basic_json_fmt)
+
+    logger = logging.getLogger("webapp")
+    logger.setLevel(logging.INFO)
+    logger.addHandler(log_handler)
+
+    return logger
+
+ctr_file = "/app/logs/web-app.event.log"
+logger = create_logger(ctr_file)
 
 ###
 # Routers
@@ -11,5 +35,9 @@ async def root():
     return { "up": "webapp-1" }
 
 @app.get("/broken", status_code=403)
-async def broken():
+async def broken(request: Request):
+    headers = request.headers
+    logger.info("Request headers:")
+    for key, value in headers.items():
+        logger.info(f"Header: {key} -> {value}")
     return {"error": "Not Good"}
